324865c71639e7fbd62fed72ffc906f524aa59e5
funnygummyguy <32gsaad@slvusd.org>
2025-12-15 09:26:14 -0800

    Create input.js

---
+++ input.js
@@ -0,0 +1,9 @@
+export class Input {
+constructor() {
+this.keys = {};
+
+
+window.addEventListener('keydown', e => this.keys[e.key.toLowerCase()] = true);
+window.addEventListener('keyup', e => this.keys[e.key.toLowerCase()] = false);
+}
+}
